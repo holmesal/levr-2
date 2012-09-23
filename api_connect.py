172f9a0531ff2be8ed1b5709d06dae5071eeac5e
Alonso Holmes <holmesal@bu.edu>
2012-09-23 07:13:34 -0400

    finished connect api

--- api_connect.py
+++ api_connect.py
@@ -3,62 +3,102 @@ import logging
 import api_utils
 import levr_classes as levr
 import levr_encrypt as enc
-
+from google.appengine.ext import db
 
 class ConnectFacebookHandler(webapp2.RequestHandler):
 	def get(self):
 		#RESTRICTED
 		
-		#check token
 		token = self.request.get('token')
+		uid	= self.request.get('uid')
+		#check token and uid and decrypt
 		if token == '':
 			api_utils.send_error(self,'Required parameter not passed: token')
 			return
-		#check uid
-		uid	= self.request.get('uid')
 		if uid == '':
 			api_utils.send_error(self,'Required parameter not passed: uid')
 			return
-		
 		#decrypt uid
 		try:
-			uid = enc.decrypt_key(uid)
-			uid = db.Key(uid)
+			uid = db.Key(enc.decrypt_key(uid))
 		except:
 			api_utils.send_error(self,'Invalid parameter: uid')
 			return
+		
+		#grab user
+		user = levr.Customer.get(uid)
+		#add facebook token
+		user.facebook_token = token
+		#update
+		user.put()
+		
+		#return the user
+		response = api_utils.package_user(user,'private')
+		api_utils.send_response(self,response,user)
+
 
 class ConnectFoursquareHandler(webapp2.RequestHandler):
 	def get(self):
 		#RESTRICTED
 		
-		#check token
 		token = self.request.get('token')
+		uid	= self.request.get('uid')
+		#check token and uid and decrypt
 		if token == '':
 			api_utils.send_error(self,'Required parameter not passed: token')
 			return
+		if uid == '':
+			api_utils.send_error(self,'Required parameter not passed: uid')
+			return
+		#decrypt uid
+		try:
+			uid = db.Key(enc.decrypt_key(uid))
+		except:
+			api_utils.send_error(self,'Invalid parameter: uid')
+			return
+		#grab user
+		user = levr.Customer.get(uid)
+		#add foursquare token
+		user.foursquare_token = token
+		#update
+		user.put()
+		
+		#return the user
+		response = api_utils.package_user(user,'private')
+		api_utils.send_response(self,response,user)
 		
 class ConnectTwitterHandler(webapp2.RequestHandler):
 	def get(self):
 		#RESTRICTED
 		
-		#check token
 		token = self.request.get('token')
+		uid	= self.request.get('uid')
+		#check token and uid and decrypt
 		if token == '':
 			api_utils.send_error(self,'Required parameter not passed: token')
 			return
+		if uid == '':
+			api_utils.send_error(self,'Required parameter not passed: uid')
+			return
+		#decrypt uid
+		try:
+			uid = db.Key(enc.decrypt_key(uid))
+		except:
+			api_utils.send_error(self,'Invalid parameter: uid')
+			return
 		
-class ConnectLevrHandler(webapp2.RequestHandler):
-	def get(self):
-		#RESTRICTED
+		#grab user
+		user = levr.Customer.get(uid)
+		#add twitter token
+		user.twitter_token = token
+		#update
+		user.put()
 		
-		#check token
-		token = self.request.get('token')
-		if token == '':
-			api_utils.send_error(self,'Required parameter not passed: token')
-			return	
-		
+		#return the user
+		response = api_utils.package_user(user,'private')
+		api_utils.send_response(self,response,user)
+
+
 app = webapp2.WSGIApplication([('/api/connect/facebook', ConnectFacebookHandler),
 								('/api/connect/foursquare', ConnectFoursquareHandler),
-								('/api/connect/twitter', ConnectTwitterHandler),
-								('/api/connect/levr', ConnectLevrHandler)],debug=True)
+								('/api/connect/twitter', ConnectTwitterHandler)],debug=True)
