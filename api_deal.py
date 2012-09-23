8aeca61ccf6778908ffed9609122fa924f859b1e
patrick@getlevr.com <Timeisofthe1>
2012-09-23 04:19:17 -0400

    added coments for deal API

--- api_deal.py
+++ api_deal.py
@@ -1,32 +1,118 @@
 import webapp2
 import logging
 
+'''
+DEAL OBJECT :{
+			#PUBLIC
+			dealID:
+			business:{
+					businessID:
+					businessName:
+					vicinity:
+					geoPoint:
+					geoHash:
+					}
+			dealText:
+			description:
+			shareURL: URL
+			geoPoint:
+			geoHash:
+			status:
+			largeImg: URL
+			smallImg: URL
+			dateUploaded:
+			dateEnd:
+			isExclusive: bool
+			barcode: None if empty
+			redemptions: number
+			tags: [list]
+			#PRIVATE
+			
+			}
+USER OBJECT :{
+			#PUBLIC
+			uid:
+			alias:
+			}
+'''
 
 class RedeemHandler(webapp2.RequestHandler):
 	def get(self,dealID):
+		'''
+		inputs: uid
+		Output:{
+			meta:{
+				success
+				errorMsg
+				}
+		'''
 		#RESTRICTED
 		pass
 
 class AddFavoriteHandler(webapp2.RequestHandler):
 	def get(self,dealID):
+		'''
+		Input: uid
+		Output:{
+			meta:{
+				success
+				errorMsg
+				}
+		'''
 		#RESTRICTED
 		pass
 		
 class DeleteFavoriteHandler(webapp2.RequestHandler):
 	def get(self,dealID):
+		'''
+		Input: uid
+		Output:{
+			meta:{
+				success
+				errorMsg
+				}
+		'''
 		#RESTRICTED
 		pass
 		
 class ReportHandler(webapp2.RequestHandler):
 	def get(self,dealID):
+		'''
+		Input: uid
+		Output:{
+			meta:{
+				success
+				errorMsg
+				}
+		'''
 		pass
 
 class DealImgHandler(webapp2.RequestHandler):
 	def get(self,dealID):
+		'''
+		Input: None
+		Output:{
+			meta:{
+				success
+				errorMsg
+				}
+		'''
 		pass
 
 class DealInfoHandler(webapp2.RequestHandler):
 	def get(self,dealID):
+		'''
+		Input: None
+		Output:{
+			meta:{
+				success
+				errorMsg
+				}
+			response:{
+				<DEAL OBJECT>
+				}
+			}
+		'''
 		self.response.out.write('Deal Info')
 		
 		
@@ -35,4 +121,5 @@ app = webapp2.WSGIApplication([('/api/deal/(.*)/redeem', RedeemHandler),
 								('/api/deal/(.*)/deleteFavorite', DeleteFavoriteHandler),
 								('/api/deal/(.*)/report', ReportHandler),
 								('/api/deal/(.*)/img', DealImgHandler),
-								('/api/deal/(.*)', DealInfoHandler)],debug=True)
+								('/api/deal/(.*)', DealInfoHandler)
+								],debug=True)
