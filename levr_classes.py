ca275b5e5e03b4ba00308cda1a99fcea7f9cf928
Alonso Holmes <holmesal@bu.edu>
2012-10-02 16:56:45 -0400

    silly mistake

--- levr_classes.py
+++ levr_classes.py
@@ -923,7 +923,7 @@ class Deal(polymodel.PolyModel):
 	upvotes			= db.IntegerProperty(default=0)
 	downvotes		= db.IntegerProperty(default=0)
 	pin_color		= db.StringProperty(default='D21231')
-	origin			= db.OriginProperty(default='levr')
+	origin			= db.StringProperty(default='levr')
 	karma			= db.IntegerProperty(default=0)
 
 
