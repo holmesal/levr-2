8c2dd4b2a1bfdcbcdf85e04e94c4a32df07482c0
patrick@getlevr.com <Timeisofthe1>
2012-10-14 18:03:07 -0400

    Moved spoofundeadactivity class to levr_classes where it should be. Working on exception handling for public facing handlers. Done foursquare

--- api_login.py
+++ api_login.py
@@ -155,6 +155,9 @@ class LoginTwitterHandler(webapp2.RequestHandler):
 class LoginLevrHandler(webapp2.RequestHandler):
 	@api_utils.validate(None,None,email_or_owner=True,pw=True)
 	def get(self,*args,**kwargs):
+		'''
+		A user logs in with levr using an email or alias, and a password
+		'''
 		try:
 			#RESTRICTED
 			logging.debug('LOGIN LEVR\n\n\n')
@@ -180,6 +183,11 @@ class LoginLevrHandler(webapp2.RequestHandler):
 			#create or refresh the alias
 			existing_user = levr.build_display_name(existing_user)
 			
+			
+			data = levr.SpoofUndeadNinjaActivity(existing_user).run()
+			logging.debug(levr.log_dict(data))
+			#set last login
+			existing_user = data['user']
 			#still here? update last login by putting
 			existing_user.put()
 			
@@ -201,7 +209,7 @@ class LoginValidateHandler(webapp2.RequestHandler):
 			
 			response = {'user':api_utils.package_user(user,True)}
 			
-			data = SpoofUndeadNinjaActivity(user,True).run()
+			data = levr.SpoofUndeadNinjaActivity(user).run()
 			logging.debug(levr.log_dict(data))
 			#set last login
 			user = data['user']
@@ -218,170 +226,10 @@ class Test(webapp2.RequestHandler):
 	def get(self):
 		user = levr.Customer.all().filter('email','ethan@levr.com').get()
 		
-		output = SpoofUndeadNinjaActivity(user,True).run()
+		output = levr.SpoofUndeadNinjaActivity(user).run()
 		api_utils.send_response(self,output)
 		
-class SpoofUndeadNinjaActivity:
-	'''
-	The undead walk the earth! And they like things!
-	Takes a user, calculates the time since their last login, and creates a number of 
-	notifications with a probability based on values we specify.
-	
-	variables:
-	max_likes_per_day
-	ideal_likes_per_day
-	'''
-	def __init__(self,user,development=False,**kwargs):
-		self.user = user
-		self.development = development
-		self.now = datetime.now()
-		self.now_in_seconds = long(levr.unix_time(self.now))
-		#set development params
-		if self.development:
-			deal_status = 'test'
-		else:
-			deal_status = 'active'
-		
-		#fetch all of the users active deals
-		self.user_deals				= levr.Deal.all().ancestor(self.user.key()).filter('deal_status', deal_status).fetch(None)
-		logging.debug(self.user_deals)
-		#calculate the date since last upload
-		logging.debug(self.user.date_last_login)
-		logging.debug(type(self.user.date_last_login))
-		self.days_since_last_login = self.calc_days_since(self.user.date_last_login)
-		logging.debug(self.days_since_last_login)
-		logging.debug(type(self.days_since_last_login))
-		
-		#set environment params
-		self.max_likes_per_day		= kwargs.get('max_likes_per_day',3) #a.k.a. the number of chances to like in a day
-		
-		self.ideal_likes_per_day	= float(kwargs.get('ideal_likes_per_day',2))
-		
-		#constrain system...
-		assert type(self.max_likes_per_day) == int, 'max likes per day must be int'
-		assert type(self.ideal_likes_per_day) == float or type(self.ideal_likes_per_day) == int , 'ideal_likes_per_day must be float or int'
-		assert self.max_likes_per_day > self.ideal_likes_per_day , 'Max likes per day must be greater than the ideal'
-		
-		#set the chance that the deal will be liked per iteration
-		self.chance_of_like = self.ideal_likes_per_day / self.max_likes_per_day
-		
-		
-	def run(self):
-		'''
-		WARNING: DOES NOT REPLACE THE USER WHEN RETURNING
-		'''
-		#the users accumulated notifications
-		notifications = []
-		#the accumulated undead ninjas
-		undead_ninjas_set = set()
-		for deal in self.user_deals:
-#			days_since_upload = self.calc_days_since(deal.date_created)
-#			logging.debug(days_since_upload)
-			whole_days = int(floor(self.days_since_last_login))
-			partial_days = self.days_since_last_login - whole_days
-			
-			#add up likes for whole days
-			total_likes = 0
-			for day in range(0,whole_days):
-				total_likes += self.get_likes_per_day(self.chance_of_like, self.max_likes_per_day)
-			
-			#add up likes from the partial day
-			total_likes += self.get_likes_per_day(self.chance_of_like, self.max_likes_per_day) * partial_days
-			
-			
-#			likes_per_day	= self.get_likes_per_day(self.chance_of_like, self.max_likes_per_day)
-#			logging.debug('likes per day: '+str(likes_per_day))
-#			logging.debug('total days: '+str(self.days_since_last_login))
-#			total_likes		= likes_per_day * self.days_since_last_login
-			logging.debug('total likes: '+str(total_likes))
-			#round off total likes to an integer
-			total_likes		= floor(total_likes)
-			logging.debug('total likes: '+str(total_likes))
-			
-			
-			#fetch the appropriate number of ninjas
-			undead_ninjas = api_utils.get_random_dead_ninja(total_likes)
-			logging.debug(undead_ninjas)
-			#make sure undead_ninjas is a list
-			if type(undead_ninjas) != list:
-				undead_ninjas = [undead_ninjas]
-			#add these undead ninjas to the list of undead ninjas that are being updated
-			
-			
-			#The decided number of ninjas will like the deal
-			for ninja in undead_ninjas:
-				#only add the 
-				if deal.key() not in ninja.upvotes:
-					#increase the deals upvotes
-					deal.upvotes += 1
-					#log that the undead ninja has liked this deal
-					ninja.upvotes.append(deal.key())
-					#increase the users karma
-					self.user.karma += 1
-					#update notification count
-					self.user.new_notifications += 1
-					#===========================================================
-					# Remember to change this if the create notifications functionality changed
-					#===========================================================
-					#create the notification
-					notifications.append(levr.Notification(
-										notification_type	= 'upvote',
-										line2				= random.choice(levr.upvote_phrases),
-										to_be_notified		= [self.user.key()],
-										actor				= ninja.key(),
-										deal				= deal.key(), #default to None,
-										date_in_seconds		= self.now_in_seconds
-										))
-				else:
-					pass
-			undead_ninjas_set.update(undead_ninjas)
-					
-					
-		
-		#update the users level
-		api_utils.level_check(self.user)
-		
-		db.put(notifications)
-#		db.put(self.user)
-		db.put(undead_ninjas_set)
-		
-		return {
-			'notifications':str(notifications),
-			'user':self.user,
-			'undead_ninjas':str(undead_ninjas_set)
-			}
-	def get_likes_per_day(self,chance_to_like,max_likes):
-		'''
-		Generates the number of likes per day the deal will get based on the chance that the deal will be liked in a day and the max number of likes in a day
-		
-		@param chance_to_like: The chance that the deal will be liked per simulation
-		@type chance_to_like: float 0-1
-		@param max_likes: The number of times the simulation will be run
-		@type max_likes: int
-		'''
-		likes = 0
-		for i in range(0,max_likes):
-			num = random.uniform(0,1)
-			if num <= chance_to_like:
-				likes += 1
-		return likes
-		
-		
-	
-	
-	def calc_days_since(self,date):
-		diff = self.now - date
-		logging.debug(self.now)
-		logging.debug(date)
-		logging.debug(type(date))
-		logging.debug(diff)
-		logging.debug(type(diff))
-		logging.debug(diff.total_seconds)
-		seconds_since = self.now_in_seconds - levr.unix_time(date)
-		logging.debug(seconds_since)
-		days_since = seconds_since/60./60./24.
-		logging.debug('days since: '+str(days_since))
-		return days_since
+
 	
 		
 		
--- foursquare.py
+++ foursquare.py
@@ -36,201 +36,213 @@ class AuthorizeBeginHandler(webapp2.RequestHandler):
 		
 class AuthorizeCompleteHandler(webapp2.RequestHandler):
 	def get(self):
-		logging.debug('Hit the Authorize Complete Handler')
-		client_id = 'UNHLIF5EYXSKLX50DASZ2PQBGE2HDOIK5GXBWCIRC55NMQ4C'
-		secret = 'VLKDNIT0XSA5FK3XIO05DAWVDVOXTSUHPE4H4WOHNIZV14G3'
-		redirect = 'https://levr-production.appspot.com/foursquare/authorize/complete'
-		code = self.request.get('code')
-		
-		#make request for token
-		url = "https://foursquare.com/oauth2/access_token?client_id="+client_id+"&client_secret="+secret+"&grant_type=authorization_code&redirect_uri="+redirect+"&code="+code
-		result = urlfetch.fetch(url=url)
-		token = json.loads(result.content)['access_token']
-		logging.info(token)
-		
-		#grab more user details
-		url = 'https://api.foursquare.com/v2/users/self?v=20120920&oauth_token='+token
-		result = urlfetch.fetch(url=url)
-		response_dict = json.loads(result.content)
-		
-		logging.debug(levr.log_dict(response_dict))
-		
-		#let the foursquare parsing code do its thing
-		user = social.Foursquare(foursquare_token=token)
-		
-		user,new_details,new_friends = user.connect_with_content(response_dict,True,foursquare_token=token)
-		
-		logging.debug(levr.log_model_props(user))
-		logging.debug(levr.log_dict(new_details))
-		logging.debug(levr.log_dict(new_friends))
-		
-		
-		#send the founders a text
-		levr.text_notify(user.first_name + ' ' + user.last_name + ' from foursquare')
-		
-		#launch the jinja environment
-		jinja_environment = jinja2.Environment(loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
-		#set up the jinja template and echo out
-		template = jinja_environment.get_template('templates/landing.html')
-		self.response.out.write(template.render())
-		
-		logging.debug(levr.log_dict(user))
+		try:
+			logging.debug('Hit the Authorize Complete Handler')
+			client_id = 'UNHLIF5EYXSKLX50DASZ2PQBGE2HDOIK5GXBWCIRC55NMQ4C'
+			secret = 'VLKDNIT0XSA5FK3XIO05DAWVDVOXTSUHPE4H4WOHNIZV14G3'
+			redirect = 'https://levr-production.appspot.com/foursquare/authorize/complete'
+			code = self.request.get('code')
+			
+			#make request for token
+			url = "https://foursquare.com/oauth2/access_token?client_id="+client_id+"&client_secret="+secret+"&grant_type=authorization_code&redirect_uri="+redirect+"&code="+code
+			result = urlfetch.fetch(url=url)
+			token = json.loads(result.content)['access_token']
+			logging.info(token)
+			
+			#grab more user details
+			url = 'https://api.foursquare.com/v2/users/self?v=20120920&oauth_token='+token
+			result = urlfetch.fetch(url=url)
+			response_dict = json.loads(result.content)
+			
+			logging.debug(levr.log_dict(response_dict))
+			
+			#let the foursquare parsing code do its thing
+			user = social.Foursquare(foursquare_token=token)
+			
+			user,new_details,new_friends = user.connect_with_content(response_dict,True,foursquare_token=token)
+			
+			logging.debug(levr.log_model_props(user))
+			logging.debug(levr.log_dict(new_details))
+			logging.debug(levr.log_dict(new_friends))
+			
+			
+			#send the founders a text
+			levr.text_notify(user.first_name + ' ' + user.last_name + ' from foursquare')
+			
+			#launch the jinja environment
+			jinja_environment = jinja2.Environment(loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
+			#set up the jinja template and echo out
+			template = jinja_environment.get_template('templates/landing.html')
+			self.response.out.write(template.render())
+			
+			logging.debug(levr.log_dict(user))
+		except:
+			levr.log_error()
+			self.response.out.write('Could not connect with Foursquare')
 
 class PushHandler(webapp2.RequestHandler):
 	def post(self):
-		logging.debug('Foursquare push request received!')
-		logging.debug(self.request.body)
-		checkin = json.loads(self.request.get('checkin'))
-		secret = self.request.get('secret')
-		logging.debug(checkin)
-		
-		#verify that the secret passed matches ours
-		hc_secret = 'VLKDNIT0XSA5FK3XIO05DAWVDVOXTSUHPE4H4WOHNIZV14G3'
-		if hc_secret != secret:
-			#raise an exception
-			logging.debug('SECRETS DO NOT MATCH')
-		
-		#go look in our database for a matching foursquare venue id
-		business = levr.Business.gql('WHERE foursquare_id = :1',checkin["venue"]["id"]).get()
-		#business = levr.Business.get('ahFzfmxldnItcHJvZHVjdGlvbnIQCxIIQnVzaW5lc3MY-dIBDA')
-		user = levr.Customer.gql('WHERE foursquare_id = :1',int(checkin['user']['id'])).get()
-		
-		#initialize the response object - these are defaults
-		contentID = levr.create_content_id('foursquare')
-		levr_url = 'http://www.levr.com/mobile/'+contentID
-		
-		#search for expired offers at that business
-		#q_expired = levr.Deal.gql('WHERE businessID=:1 AND deal_status=:2',business)
-		
-		if business:	#business found - CURRENTLY ONLY REPLYING AT BUSINESSES THAT ARE IN OUR DATABASE
-			#for deal in levr.Deal().all().filter('businessID =', str(business.key())).run():
-			q = levr.Deal.gql("WHERE businessID = :1 AND deal_status = :2 ORDER BY count_redeemed DESC",str(business.key()),'active')
+		try:
+			logging.debug('Foursquare push request received!')
+			logging.debug(self.request.body)
+			checkin = json.loads(self.request.get('checkin'))
+			secret = self.request.get('secret')
+			logging.debug(checkin)
 			
-			deal = None
+			#verify that the secret passed matches ours
+			hc_secret = 'VLKDNIT0XSA5FK3XIO05DAWVDVOXTSUHPE4H4WOHNIZV14G3'
+			if hc_secret != secret:
+				#raise an exception
+				logging.debug('SECRETS DO NOT MATCH')
 			
-			for d in q:
-				if d.origin != 'foursquare':
-					deal = d
-					text = 'Click to view the most popular offer here: '+ deal.deal_text
-					action='deal'
-					break
-			else:
-				text = "See any deals? Pay it forward! Click to add."
-				action = 'upload'
-				deal = None
-
-#	
-# 			
-# 			numdeals = q.count()
-# 			if numdeals > 0:	#many deals found
-# 				deal_matched = False
-# 				text = "See any deals? Pay it forward! Click to add."
-# 				action = 'upload'
-# 				while deal_matched == False:
-# 					deal = q.get()
-# 					if deal.origin != 'foursquare':
-# 						text = 'Click to view the most popular offer here: '+ deal.deal_text
-# 						action='deal'
-# 						deal_matched = True
-# 			else:	#no deals found
-# 				text = "See any deals? Pay it forward! Click to add."
-# 				action = 'upload'
-		
-			#create floating_content entity and put
-			floating_content = levr.FloatingContent(
-				action=action,
-				origin='foursquare',
-				contentID=contentID,
-				user=user,
-				deal=deal,
-				business=business
-			)
-			floating_content.put()
+			#go look in our database for a matching foursquare venue id
+			business = levr.Business.gql('WHERE foursquare_id = :1',checkin["venue"]["id"]).get()
+			#business = levr.Business.get('ahFzfmxldnItcHJvZHVjdGlvbnIQCxIIQnVzaW5lc3MY-dIBDA')
+			user = levr.Customer.gql('WHERE foursquare_id = :1',int(checkin['user']['id'])).get()
 			
+			#initialize the response object - these are defaults
+			contentID = levr.create_content_id('foursquare')
+			levr_url = 'http://www.levr.com/mobile/'+contentID
 			
-			reply = {
-				'CHECKIN_ID'		:checkin['id'],
-				'text'				:text,
-				'url'				:levr_url,
-				'contentId'			:contentID
-			}
-		
-		
-		
-			'''else:			#no business found
-				#ask pat for all the deals within walking distance
-				url = 'http://www.levr.com/phone'
-				ll = str(checkin['venue']['location']['lat'])+','+str(checkin['venue']['location']['lng'])
-				request_point = levr.geo_converter(ll)
-				precision = 6
-				results = levr_utils.get_deals_in_area(['all'],request_point,precision)
-	
-				if len(results) > 0:
-					reply['text'] = "There are "+str(len(results))+" deals near you - click to view."
-					reply['url'] = '' #deeplink into deal upload screen
-				else:
-					reply['text'] = "See any deals? Pay it forward: click to upload."
-					reply['url'] = '' #deeplink into deal upload screen'''
-					
+			#search for expired offers at that business
+			#q_expired = levr.Deal.gql('WHERE businessID=:1 AND deal_status=:2',business)
 			
-			#reply['CHECKIN_ID'] = checkin['id']
-			#reply['text'] = 'Hey ethan. click here to see this reply inside Levr.'
-			#reply['url']  = 'fsq+unhlif5eyxsklx50dasz2pqbge2hdoik5gxbwcirc55nmq4c+reply://?contentId=abcdefg12345&fsqCallback=foursquare://checkins/'+checkin['id']
-			#reply['url']  = 'fsq+unhlif5eyxsklx50dasz2pqbge2hdoik5gxbwcirc55nmq4c+reply://?contentId=abcdefg12345'
-			#reply['url']  = 'http://www.levr.com?contentId=abcdefg12345'
-			#reply['url']  = 'http://www.levr.com/deal/blah'
-			#reply['contentId'] = 'abcdefg12345'
+			if business:	#business found - CURRENTLY ONLY REPLYING AT BUSINESSES THAT ARE IN OUR DATABASE
+				#for deal in levr.Deal().all().filter('businessID =', str(business.key())).run():
+				q = levr.Deal.gql("WHERE businessID = :1 AND deal_status = :2 ORDER BY count_redeemed DESC",str(business.key()),'active')
 				
-			url = 'https://api.foursquare.com/v2/checkins/'+checkin['id']+'/reply?v=20120920&oauth_token='+user.foursquare_token
-			#url = 'https://api.foursquare.com/v2/checkins/'+reply['CHECKIN_ID']+'/reply?v=20120920&text=hitherehello&url=foursquare%2Bunhlif5eyxsklx50dasz2pqbge2hdoik5gxbwcirc55nmq4c%2Breply%3A//%3FcontentId%3Dabcdefg12345&contentId=abcdefg12345&oauth_token='+'PZVIKS4EH5IFBJX1GH5TUFYAA3Z5EX55QBJOE3YDXKNVYESZ'
-			logging.debug(url)
-			#result = urlfetch.fetch(url=url,method=urlfetch.POST)
-			result = urlfetch.fetch(url=url,
-									payload=urllib.urlencode(reply),
-									method=urlfetch.POST)
-			logging.debug(levr.log_dict(result.__dict__))
-		else:
-			logging.info('BUSINESS NOT FOUND')
+				deal = None
+				
+				for d in q:
+					if d.origin != 'foursquare':
+						deal = d
+						text = 'Click to view the most popular offer here: '+ deal.deal_text
+						action='deal'
+						break
+				else:
+					text = "See any deals? Pay it forward! Click to add."
+					action = 'upload'
+					deal = None
+	
+	#	
+	# 			
+	# 			numdeals = q.count()
+	# 			if numdeals > 0:	#many deals found
+	# 				deal_matched = False
+	# 				text = "See any deals? Pay it forward! Click to add."
+	# 				action = 'upload'
+	# 				while deal_matched == False:
+	# 					deal = q.get()
+	# 					if deal.origin != 'foursquare':
+	# 						text = 'Click to view the most popular offer here: '+ deal.deal_text
+	# 						action='deal'
+	# 						deal_matched = True
+	# 			else:	#no deals found
+	# 				text = "See any deals? Pay it forward! Click to add."
+	# 				action = 'upload'
+			
+				#create floating_content entity and put
+				floating_content = levr.FloatingContent(
+					action=action,
+					origin='foursquare',
+					contentID=contentID,
+					user=user,
+					deal=deal,
+					business=business
+				)
+				floating_content.put()
+				
+				
+				reply = {
+					'CHECKIN_ID'		:checkin['id'],
+					'text'				:text,
+					'url'				:levr_url,
+					'contentId'			:contentID
+				}
+			
+			
+			
+				'''else:			#no business found
+					#ask pat for all the deals within walking distance
+					url = 'http://www.levr.com/phone'
+					ll = str(checkin['venue']['location']['lat'])+','+str(checkin['venue']['location']['lng'])
+					request_point = levr.geo_converter(ll)
+					precision = 6
+					results = levr_utils.get_deals_in_area(['all'],request_point,precision)
+		
+					if len(results) > 0:
+						reply['text'] = "There are "+str(len(results))+" deals near you - click to view."
+						reply['url'] = '' #deeplink into deal upload screen
+					else:
+						reply['text'] = "See any deals? Pay it forward: click to upload."
+						reply['url'] = '' #deeplink into deal upload screen'''
+						
+				
+				#reply['CHECKIN_ID'] = checkin['id']
+				#reply['text'] = 'Hey ethan. click here to see this reply inside Levr.'
+				#reply['url']  = 'fsq+unhlif5eyxsklx50dasz2pqbge2hdoik5gxbwcirc55nmq4c+reply://?contentId=abcdefg12345&fsqCallback=foursquare://checkins/'+checkin['id']
+				#reply['url']  = 'fsq+unhlif5eyxsklx50dasz2pqbge2hdoik5gxbwcirc55nmq4c+reply://?contentId=abcdefg12345'
+				#reply['url']  = 'http://www.levr.com?contentId=abcdefg12345'
+				#reply['url']  = 'http://www.levr.com/deal/blah'
+				#reply['contentId'] = 'abcdefg12345'
+					
+				url = 'https://api.foursquare.com/v2/checkins/'+checkin['id']+'/reply?v=20120920&oauth_token='+user.foursquare_token
+				#url = 'https://api.foursquare.com/v2/checkins/'+reply['CHECKIN_ID']+'/reply?v=20120920&text=hitherehello&url=foursquare%2Bunhlif5eyxsklx50dasz2pqbge2hdoik5gxbwcirc55nmq4c%2Breply%3A//%3FcontentId%3Dabcdefg12345&contentId=abcdefg12345&oauth_token='+'PZVIKS4EH5IFBJX1GH5TUFYAA3Z5EX55QBJOE3YDXKNVYESZ'
+				logging.debug(url)
+				#result = urlfetch.fetch(url=url,method=urlfetch.POST)
+				result = urlfetch.fetch(url=url,
+										payload=urllib.urlencode(reply),
+										method=urlfetch.POST)
+				logging.debug(levr.log_dict(result.__dict__))
+			else:
+				logging.info('BUSINESS NOT FOUND')
+		except:
+			levr.log_error()
+			self.reponse.out.write('Could not connect with Foursquare')
 		
 class CatchUpHandler(webapp2.RequestHandler):
 	def get(self):
-		client_id = 'HD3ZXKL5LX4TFCARNIZO1EG2S5BV5UHGVDVEJ2AXB4UZHEOU'
-		secret = 'LB3J4Q5VQWZPOZATSMOAEDOE5UYNL5P44YCR0FCPWFNXLR2K'
-		
-		#scan the database for entries with no foursquare ID
-		q = levr.Business.all().filter('foursquare_id =','undefined')
-		
-		for business in q.run():
-			ll = str(business.geo_point)
-			search = urllib.quote(business.business_name)
-			url = "https://api.foursquare.com/v2/venues/search?v=20120920&intent=match&ll="+ll+"&query="+search+"&client_id="+client_id+"&client_secret="+secret
-			result = urlfetch.fetch(url=url)
+		try:
+			client_id = 'HD3ZXKL5LX4TFCARNIZO1EG2S5BV5UHGVDVEJ2AXB4UZHEOU'
+			secret = 'LB3J4Q5VQWZPOZATSMOAEDOE5UYNL5P44YCR0FCPWFNXLR2K'
 			
-			self.response.out.write('running ' + business.business_name + '\n')
+			#scan the database for entries with no foursquare ID
+			q = levr.Business.all().filter('foursquare_id =','undefined')
 			
-			try:
-				match = json.loads(result.content)['response']['venues'][0]
-				business.foursquare_id = match['id']
-				business.foursquare_name = match['name']
-				business.put()
-				logging.debug('added foursquare data for ' + business.business_name)
-				#logging.debug(business.business_name + ' REPLACED BY ' + match['name'])
-			except:
-				business.foursquare_id = 'notfound'
-				business.foursquare_name = 'notfound'
-				business.put()
-				logging.debug('no match found for ' + business.business_name)
+			for business in q.run():
+				ll = str(business.geo_point)
+				search = urllib.quote(business.business_name)
+				url = "https://api.foursquare.com/v2/venues/search?v=20120920&intent=match&ll="+ll+"&query="+search+"&client_id="+client_id+"&client_secret="+secret
+				result = urlfetch.fetch(url=url)
 				
-		
-		'''
-		search = self.request.get('q')
-		logging.info(search)
-		url = "https://api.foursquare.com/v2/venues/search?v=20120920&intent=match&ll=42.351824,-71.11982&query="+urllib.quote(search)+"&client_id="+client_id+"&client_secret="+secret
-		logging.info(url)
-		result = urlfetch.fetch(url=url)
-		business = json.loads(result.content)
-		self.response.out.write(levr_utils.log_dict(business))
-		'''
+				self.response.out.write('running ' + business.business_name + '\n')
+				
+				try:
+					match = json.loads(result.content)['response']['venues'][0]
+					business.foursquare_id = match['id']
+					business.foursquare_name = match['name']
+					business.put()
+					logging.debug('added foursquare data for ' + business.business_name)
+					#logging.debug(business.business_name + ' REPLACED BY ' + match['name'])
+				except:
+					business.foursquare_id = 'notfound'
+					business.foursquare_name = 'notfound'
+					business.put()
+					logging.debug('no match found for ' + business.business_name)
+					
+			
+			'''
+			search = self.request.get('q')
+			logging.info(search)
+			url = "https://api.foursquare.com/v2/venues/search?v=20120920&intent=match&ll=42.351824,-71.11982&query="+urllib.quote(search)+"&client_id="+client_id+"&client_secret="+secret
+			logging.info(url)
+			result = urlfetch.fetch(url=url)
+			business = json.loads(result.content)
+			self.response.out.write(levr_utils.log_dict(business))
+			'''
+		except:
+			levr.log_error()
+			self.response.out.write('Could not connect with Foursquare')
 
 class TestHandler(webapp2.RequestHandler):
 	def get(self):
