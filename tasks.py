e1a7507f99f573edd1333ca05c9f4b296430c3df
pnw <patrick@levr.com>
2012-11-30 14:32:03 -0500

    removed some foursquare tasks

--- tasks.py
+++ tasks.py
@@ -17,36 +17,36 @@ class SearchFoursquareTaskHandler(webapp2.RequestHandler):
 		try:
 			logging.info('''
 				
-				THE FOURSQUARE DEAL SEARCH TASK IS RUNNING
+				THE FOURSQUARE DEAL SEARCH TASK IS RUNNING. Jk. No it's not anymore
 				
 				''')
-			
-			
-			
-#  			logging.debug(self.request.body)
-			payload = json.loads(self.request.body)
-# 			logging.debug(payload)
-			geo_point = db.GeoPt(payload['lat'],payload['lon'])
-# 			logging.info(geo_point)
-			foursquare_ids = payload['foursquare_ids']
-# 			logging.info(foursquare_ids)
-			#grab the token from the payload
-			token = payload['token']
-#			logging.info(token)
-			deal_status = payload.get('deal_status','active')
-# 			logging.info(levr.log_model_props(user))
-			
-			#foursquare stuff!
-			ft1 = datetime.now()
-#			logging.info('FOURSQUARE IDS::::')
-#			logging.info(foursquare_ids)
-#			logging.debug('searching foursquare () from a task!')
-			foursquare_deals = api_utils.search_foursquare(geo_point,token,deal_status,foursquare_ids)
-			logging.info('Foursquare results found: '+str(len(foursquare_deals)))
-#			logging.info('Duplicates not put in database: '+str(len(foursquare_ids)))
-			ft2 = datetime.now()
-			logging.info('Foursquare action time: ' + str(ft2-ft1))
-			
+#			
+#			
+#			
+##  			logging.debug(self.request.body)
+#			payload = json.loads(self.request.body)
+## 			logging.debug(payload)
+#			geo_point = db.GeoPt(payload['lat'],payload['lon'])
+## 			logging.info(geo_point)
+#			foursquare_ids = payload['foursquare_ids']
+## 			logging.info(foursquare_ids)
+#			#grab the token from the payload
+#			token = payload['token']
+##			logging.info(token)
+#			deal_status = payload.get('deal_status','active')
+## 			logging.info(levr.log_model_props(user))
+#			
+#			#foursquare stuff!
+#			ft1 = datetime.now()
+##			logging.info('FOURSQUARE IDS::::')
+##			logging.info(foursquare_ids)
+##			logging.debug('searching foursquare () from a task!')
+#			foursquare_deals = api_utils.search_foursquare(geo_point,token,deal_status,foursquare_ids)
+#			logging.info('Foursquare results found: '+str(len(foursquare_deals)))
+##			logging.info('Duplicates not put in database: '+str(len(foursquare_ids)))
+#			ft2 = datetime.now()
+#			logging.info('Foursquare action time: ' + str(ft2-ft1))
+#			
 			
 		except:
 			levr.log_error()
@@ -56,58 +56,58 @@ class BusinessHarmonizationTaskHandler(webapp2.RequestHandler):
 		try:
 			logging.info('''
 			
-			THE FOURSQUARE BUSINESS TASK IS RUNNING
+			THE FOURSQUARE BUSINESS TASK IS RUNNING. Jk. No it's not anymore
 			
 			''')
 			
-			payload = json.loads(self.request.body)
-			logging.debug(payload)
-			
-			geo_point = levr.geo_converter(payload['geo_str'])
-			
-			query = payload['query']
-			
-			key = payload['key']
-			
-			match = api_utils.match_foursquare_business(geo_point,query)
-	
-			logging.info(match)
-			
-			business = levr.Business.get(key)
-			
-			if match:
-				logging.info('Foursquare ID found: '+match['foursquare_id'])
-				
-				#are there any previously added foursquare businesses?
-				#q_orphans = levr.Business.gql('WHERE foursquare_id=:1',match['foursquare_id'])
-				
-				#grab a duplicate business
-				duplicate_business = levr.Business.gql('WHERE foursquare_id=:1',match['foursquare_id']).get()
-				
-				keys = []
-				if duplicate_business:
-					#grab all the deal keys from that business
-					keys = levr.Deal.gql('WHERE businessID = :1',str(duplicate_business.key())).fetch(None,keys_only=True)
+#			payload = json.loads(self.request.body)
+#			logging.debug(payload)
+#			
+#			geo_point = levr.geo_converter(payload['geo_str'])
+#			
+#			query = payload['query']
+#			
+#			key = payload['key']
+#			
+#			match = api_utils.match_foursquare_business(geo_point,query)
+#	
+#			logging.info(match)
+#			
+#			business = levr.Business.get(key)
+#			
+#			if match:
+#				logging.info('Foursquare ID found: '+match['foursquare_id'])
+#				
+#				#are there any previously added foursquare businesses?
+#				#q_orphans = levr.Business.gql('WHERE foursquare_id=:1',match['foursquare_id'])
+#				
+#				#grab a duplicate business
+#				duplicate_business = levr.Business.gql('WHERE foursquare_id=:1',match['foursquare_id']).get()
+#				
+#				keys = []
+#				if duplicate_business:
+#					#grab all the deal keys from that business
+#					keys = levr.Deal.gql('WHERE businessID = :1',str(duplicate_business.key())).fetch(None,keys_only=True)
+##					duplicate_business.delete()
+#				
+#				#update business entity
+#				business.foursquare_id = match['foursquare_id']
+#				business.foursquare_name = match['foursquare_name']
+#				business.foursquare_linked	=	True
+#				business.put()
+#				
+#				deals = list(set(db.get(keys)))
+#				for deal in deals:
+#					deal.businessID = str(business.key())
+#					deal.business = business
+#					logging.debug('UPDATED DEAL BUSINESSID')
+#				db.put(deals)
+#				if duplicate_business:
 #					duplicate_business.delete()
-				
-				#update business entity
-				business.foursquare_id = match['foursquare_id']
-				business.foursquare_name = match['foursquare_name']
-				business.foursquare_linked	=	True
-				business.put()
-				
-				deals = list(set(db.get(keys)))
-				for deal in deals:
-					deal.businessID = str(business.key())
-					deal.business = business
-					logging.debug('UPDATED DEAL BUSINESSID')
-				db.put(deals)
-				if duplicate_business:
-					duplicate_business.delete()
-					logging.debug('DELETED ORIGINAL FOURSQUARE BUSINESS')
-			else:
-				#update to show notfound
-				logging.info('No foursquare match found.')
+#					logging.debug('DELETED ORIGINAL FOURSQUARE BUSINESS')
+#			else:
+#				#update to show notfound
+#				logging.info('No foursquare match found.')
 				
 				
 			#go grab all the deals and update
@@ -120,26 +120,26 @@ class FoursquareDealUpdateTaskHandler(webapp2.RequestHandler):
 		try:
 			logging.info('''
 			
-			THE FOURSQUARE DEAL UPDATE TASK IS RUNNING
+			THE FOURSQUARE DEAL UPDATE TASK IS RUNNING. Jk no its not anymore.
 			
 			''')
 			
-			payload = json.loads(self.request.body)
-			
-			foursquare_id = payload['foursquare_id']
-			deal_id = payload['deal_id']
-			uid = payload['uid']
-			token =  payload['token']
-			deal_status = payload['deal_status']
-			
-			logging.info('This task was started by a user/deal with the following information:')
-			logging.info('UID: '+uid)
-			logging.info('Foursquare user token: '+token)
-			logging.info('Reported deal ID: '+deal_id)	
-			logging.info('Business foursquare ID: '+foursquare_id)
-			logging.info('deal_status: '+deal_status)
-			
-			api_utils.update_foursquare_business(foursquare_id,deal_status,token)
+#			payload = json.loads(self.request.body)
+#			
+#			foursquare_id = payload['foursquare_id']
+#			deal_id = payload['deal_id']
+#			uid = payload['uid']
+#			token =  payload['token']
+#			deal_status = payload['deal_status']
+#			
+#			logging.info('This task was started by a user/deal with the following information:')
+#			logging.info('UID: '+uid)
+#			logging.info('Foursquare user token: '+token)
+#			logging.info('Reported deal ID: '+deal_id)	
+#			logging.info('Business foursquare ID: '+foursquare_id)
+#			logging.info('deal_status: '+deal_status)
+#			
+#			api_utils.update_foursquare_business(foursquare_id,deal_status,token)
 			
 			
 		except:
@@ -165,7 +165,7 @@ class NewUserTextTaskHandler(webapp2.RequestHandler):
 			auth_header = 'Basic '+base64.b64encode(sid+':'+token)
 			logging.info(auth_header)
 			
-			numbers = ['+16052610083','+16035605655','+16173124536','+16173124536','+12036329029']
+			numbers = ['+16052610083','+16035605655','+16173124536','+12036329029']
 			
 			for number in numbers:
 				request = {'From':'+16173608582',
